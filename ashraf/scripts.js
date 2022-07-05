fd0c6d263956189e43ae1739d7b45aa9cb611f2c
ashrafp216 <86481822+ashrafp216@users.noreply.github.com>
2022-07-05 03:44:19 -0700

    Add files via upload

---
+++ ashraf/scripts.js
@@ -0,0 +1,30 @@
+var nav = document.getElementById('nav'); // Identify target
+var nav
+
+window.addEventListener('scroll', function (event) { // To listen for event
+    event.preventDefault();
+
+    if (window.scrollY <= 150) { // Just an example
+        // nav.style.backgroundColor = 'transparent'; // or default color
+        nav.classList.add('nav-normal')
+        nav.classList.remove('nav-scrolled')
+    } else {
+        // nav.style.backgroundColor = 'color'
+        nav.classList.add('nav-scrolled')
+        nav.classList.remove('nav-normal')
+
+    }
+});
+
+function ScrollRight() {
+
+    document.getElementById("initiatives-container").scrollLeft += 800;
+} 
+function ScrollLeft() {
+
+    document.getElementById("initiatives-container").scrollLeft -= 800;
+}
+function scrollToView(e) {
+    document.getElementById('initiatives-container').scrollLeft  = e.target.offsetLeft
+    document.getElementById('initiatives-container').ScrollRight  = e.target.offsetRight
+}
