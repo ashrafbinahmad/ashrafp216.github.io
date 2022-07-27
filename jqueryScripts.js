9c326834c5d62498a32bba971f4acabad009ca92
ashrafp216 <86481822+ashrafp216@users.noreply.github.com>
2022-07-27 04:26:28 -0700

    .

--- jqueryScripts.js
+++ jqueryScripts.js
@@ -46,7 +46,8 @@ function submit(event) {
 }
 
 const validateEmail = (email) => {
-    if (email.test(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+    emailModel = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (emailModel.test(email)) {
         return String(email).toLowerCase()
     } else {
         return false
