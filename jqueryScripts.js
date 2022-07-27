edab5715dda2592bffd5bf17354e53b7258ab30d
ashrafp216 <86481822+ashrafp216@users.noreply.github.com>
2022-07-27 04:24:26 -0700

    .

--- jqueryScripts.js
+++ jqueryScripts.js
@@ -7,7 +7,26 @@ $("#aboutbtn").click(function () {
     return false;
 });
 
-$('#submit').click(function (event) {
+// $('#submit').click(function (event) {
+//     event.preventDefault();
+//     let myForm = document.getElementById("subscribe_form");
+//     let email = document.getElementById("email_input").value;
+//     console.log(email)
+//     if (validateEmail(email)) {
+//         console.log('null value');
+//         return;
+//     }
+//     let formData = new FormData(myForm);
+//     fetch("/", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//         body: new URLSearchParams(formData).toString(),
+//     })
+//         .then(() => console.log("Form successfully submitted"))
+//         .catch((error) => alert(error));
+// })
+
+function submit(event) {
     event.preventDefault();
     let myForm = document.getElementById("subscribe_form");
     let email = document.getElementById("email_input").value;
@@ -24,7 +43,7 @@ $('#submit').click(function (event) {
     })
         .then(() => console.log("Form successfully submitted"))
         .catch((error) => alert(error));
-})
+}
 
 const validateEmail = (email) => {
     if (email.test(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
