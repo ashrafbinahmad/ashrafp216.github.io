8ae0fd7eecdacf06d3149b9583aa748787f0ab34
ashrafp216 <86481822+ashrafp216@users.noreply.github.com>
2022-07-29 05:55:39 -0700

    page links check

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 data = {
 
     pages: [
-        { name: 'About us', link: '/about_us' },
-        { name: 'Events', link: '/events' },
-        { name: 'Services', link: '/services' },
+        { name: 'About us', link: 'about_us', local_link: "about_us\\index.html" },
+        { name: 'Events', link: 'events', local_link: "about_us\\index.html" },
+        { name: 'Services', link: 'services', local_link: "about_us\\index.html" },
     ],
     menu_items: [
         { name: 'About us', link: '#aboutus' },
