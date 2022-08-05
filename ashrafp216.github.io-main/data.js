75c390ec89024cde6afc31bad1a8f33ca995d69d
ashrafp216 <86481822+ashrafp216@users.noreply.github.com>
2022-08-05 10:34:04 -0700

    Add files via upload

---
+++ ashrafp216.github.io-main/data.js
@@ -0,0 +1,85 @@
+data = {
+    pages: [
+        { name: 'About us',title:"About us  <i class='fa fa-angle-right'></i>", link: 'about_us.html', local_link: "about_us\\index.html", image: "images/about_us.png" },
+        { name: 'Events',title:"Events  <i class='fa fa-angle-right'></i>", link: 'events.html', local_link: "events\\index.html", image: "images/events.png" },
+        { name: 'Services',title:"Services  <i class='fa fa-angle-right'></i>", link: 'services.html', local_link: "services\\index.html", image: "images/services.png" },
+        { name: 'Archieves',title:"Archieves  <i class='fa fa-angle-right'></i>", link: 'archieves.html', local_link: "archieves\\index.html", image: "images/archieves.png" },
+    ],
+    menu_items: [
+        { name: 'About us', link: '#aboutus' },
+        { name: 'Events', link: '#events' },
+    ],
+
+    tabs: {
+        items: [
+            "About the course",
+            "Program Structure",
+            "Student life",
+            "Placement and Career",
+        ],
+        about: "Master of Islamic Economics and Finance is two year PG course provided by Darul Huda Islamic University. Our course introduces and develops knowledge of the students in various specialised areas across a range of core subjects as well as giving solid foundation in Islamic nance principles, providing the skills need to move into wide variety of careers after graduation. Through a variety of modules, the students can learn about the ethos, principles and practices of islamic nance. The course also enables to pursue specialisation in several important areas of Islamic nance   <br> <br> including Islamic banking operations and currency system in Islam. The mixed philosophy of the program enables the graduate to attain the knowledge needed to understand the global nancial system and to propose viable alternatives to the existing conventional models by blending guidance from Sharia with modern scientic knowledge.  ",
+        Program_Structure: "The program is designed for two years, full time, post graduate degree organized in four semesters. <br><br> <small> <i class='fa fa-solid fa-circle'></i> </small>  Students with thesis mode should cover four specialization papers from their department and only two (one in one semester) from their sister department of the kulliya within four semesters.    <br><br>  <small> <i class='fa fa-solid fa-circle'></i> </small>   Students with course only mode should cover six specialization papers from their department and only two (one in one semester) from their sister department of the kulliya within four semesters. <br><br> Students are also requested to cover ve block papers (Qur'an, Hadith, Fiqh, General and Elective) consisting of 19 papers within four semesters. In Qur'an block students have to cover only three papers while in the Elective block they can enjoy the selection of 4 papers from 8 provided papers. <br> Taking a paper from each block except from the Qur'an block is mandatory   ",
+        Student_life: "Department of Islamic economics and nance is a centre of constructive, intellectual and productive dialogues and academic ventures in the University. Earnest students in the department have vibrant avenues to explore new trends in the global nancial system, connect with prominent academicians and thrive in diverse elds. Department is a great place to gear up your academic enthusiasm through multifarious engagements ranging from authoring books, writing journal articles, participating in academic conferences, organising seminars, webinars, and workshops. <br><br> The department provides a bounteous scope of extracurricular activities for the holistic development of students and encourages them to make the most of their experience on campus grounded in Islamic values. For the accomplishment of academic instincts of the department, the department facilitates two weeks long national and international eld trips. In the academic eld visits, students can acquire rst hand experiences of multiple Islamic nancial institutions such as Islamic banks and interact with prominent academicians and scholars working in this eld.  ",
+        Placement: "Graduates of this course will be equipped to Undertake various methods of research and produce authentic thesis works and dessertations on various topics related to Islamic economics and nance. <br> <br>  <small> <i class='fa fa-solid fa-circle'></i> </small> Demonstrate expertise in multiple elds within the Islamic economics and nance  including the theory, practice and the understanding of implementation of various  Islamic nancial products. <br><br>  <small> <i class='fa fa-solid fa-circle'></i> </small> Find out and oer new Islamic nancial products that can be applied in Islamic nancial  institutions as alternatives for those which are applied in conventional nancial  institutions. <br><br>   To look forward to acquiring job experience in countries where Islamic nance services  are being set up especially in the eld of Sharia screening and advising.  ",
+    },
+
+    lecturers: [
+        {
+            name: "Dr. Jafar Hudawi",
+            image: "images/jafar_usthad.jpg",
+            description: "Graduated from Darul Huda Islamic University, PhD in Arabic language and literature from University of Calicut.",
+            HoD: true
+        },
+        {
+            name: "KC Muhammad Baqawi",
+            image: "images/kc_usthad.jpg",
+            description: "Graduated from Madrasah Al-Baqiyath Alsalihat, Vellore.",
+            HoD: false
+        },
+        {
+            name: "Dr. Ramees Hudawi Chelembra",
+            image: "images/download.png",
+            description: "Graduated from Darul Huda Islamic University, BA, MA and PhD in economics in Calicut University.",
+            HoD: false
+        }
+    ],
+    events: [
+        {
+            name: "Isnomics",
+            image: "images/Isnomics.jpg",
+            description: "10 day campaign program conducted by the department that focused on the contributions of Islamic scholars in the held of economics.",
+            colors: { title: "#362c53", border: "#cc99ff", description: "#663399" },
+            date: "2021 September 04-14",
+        },
+
+        {
+            name: "Finaissance",
+            image: "images/coins_thasbeeh.jpg",
+            description: "Talk series program that provides the students with the opportunity to hear from prominent scholars in the field and interact with them.",
+            colors: { title: "#663300", border: "#ead3b7", description: "#cc6600" },
+            date: "2021 September 01",
+        },
+        {
+            name: "Seyaha",
+            image: "images/Seyaha.jpg",
+            description: "Two weeks long academic trip through which the students can visit Islamic financial institutions and understand the operations going on there.",
+            colors: { title: "#402382", border: "#cc99ff", description: "#402382" },
+            date: "2021 September 01",
+        },
+        {
+            name: "Seminars and <br> Webinars",
+            image: "images/waqf_seminar.jpg",
+            description: "Waqf Seminar  And more... <br><br><br>",
+            colors: { title: "#663300", border: "#ead3b7", description: "#cc6600" },
+            date: "2021 September 01",
+        },
+    ],
+    dept_committee: [
+        {
+            name: "",
+            image: "",
+            role: ""
+        },
+    ]
+
+};
---
+++ ashrafp216.github.io-main/scripts.js
@@ -0,0 +1,138 @@
+gsap.registerPlugin(Flip);
+AOS.init();
+const firebaseConfig = {
+    apiKey: "AIzaSyBcXNbJFY2KNxt4DXi-fIrJuyU7ksH8-ak",
+    authDomain: "iefwebsite.firebaseapp.com",
+    databaseURL: "https://iefwebsite-default-rtdb.firebaseio.com",
+    projectId: "iefwebsite",
+    storageBucket: "iefwebsite.appspot.com",
+    messagingSenderId: "1005055631588",
+    appId: "1:1005055631588:web:3324c5f4f8470c5258ccf1",
+    measurementId: "G-YJREGCGRLK"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+screen.width
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 80,
+    centeredSlides: true,
+
+    draggable: true,
+    loop: false,
+    keyboard: {
+        enabled: true,
+    },
+
+
+    // autoplay: {
+    //     delay: 4000,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+
+    },
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+    }, breakpoints: {
+        // when window width is >= 320px
+        0: {
+            slidesPerView: 1,
+
+        },
+        1276: {
+            slidesPerView: 2,
+            effect: 'coverflow',
+            coverflowEffect: {
+                rotate: 0,
+                slideShadows: false,
+            },
+        },
+    }
+});
+
+function SaveToDb(data, ref, action) {
+    firebase.database().ref(ref).push(data).then(action);
+}
+
+var nav = document.getElementById('nav');
+
+window.addEventListener('scroll', function (event) {
+    event.preventDefault();
+
+    if (window.scrollY <= 150) {
+        nav.classList.add('nav-normal')
+        nav.classList.remove('nav-scrolled')
+    } else {
+        nav.classList.add('nav-scrolled')
+        nav.classList.remove('nav-normal')
+
+    }
+});
+
+function AutoPrepent_0(num) {
+    if (num.toString().length = 1) {
+        return '0' + num.toString()
+    } else return num.toString()
+}
+
+
+
+function ScrollRight() {
+
+    document.getElementById("events-container").scrollLeft += 800;
+}
+function ScrollLeft() {
+
+    document.getElementById("events-container").scrollLeft -= 800;
+}
+function scrollToView(e) {
+    document.getElementById('events-container').scrollLeft = e.target.offsetLeft
+}
+
+
+function setup() {
+    return {
+        activeTab: 0,
+        tabs: data.tabs
+    };
+};
+
+function menuData() {
+    return {
+        show_menu: false,
+        items: data.menu_items
+    }
+
+}
+
+function lecturers_data() {
+    console.log(data.lecturers)
+    return {
+        lecturers: data.lecturers
+    }
+}
+
+function eventsData() {
+    // console.log(data.events)
+    return {
+        events: data.events
+    }
+}
+function ShowMessage(message) {
+    let messageSpace = document.getElementById('messageSpace')
+    let messageSpace_parent = document.getElementById('messageSpace_parent')
+    messageSpace.innerText = message
+    messageSpace_parent.style.top = '2rem'
+    setTimeout(() => {
+        messageSpace_parent.style.top = '-5rem'
+    }, 5000)
+}
+
